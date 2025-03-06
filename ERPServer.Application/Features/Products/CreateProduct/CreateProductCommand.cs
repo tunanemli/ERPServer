99f07abf847f741383913525344ed31adbf0d079
tunanemli <tunanemli79@gmail.com>
2025-03-06 19:51:34 +0300

    Create Products işlevi eklendi

---
+++ ERPServer.Application/Features/Products/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Products.CreateProduct
+{
+    public sealed record CreateProductCommand(string Name, int TypeValue) : IRequest<Result<string>>;
+
+    public sealed class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty();
+            RuleFor(c => c.TypeValue).NotEmpty();
+        }
+    }
+}
--- ERPServer.Application/Mapping/MappingProfile.cs
+++ ERPServer.Application/Mapping/MappingProfile.cs
@@ -3,7 +3,9 @@ using ERPServer.Application.Features.Customers.CreateCustomer;
 using ERPServer.Application.Features.Customers.UpdateCustomer;
 using ERPServer.Application.Features.Depots.CreateDepot;
 using ERPServer.Application.Features.Depots.UpdateDepot;
+using ERPServer.Application.Features.Products.CreateProduct;
 using ERPServer.Domain.Entities;
+using ERPServer.Domain.Enums;
 
 namespace ERPServer.Application.Mapping
 {
@@ -13,8 +15,14 @@ namespace ERPServer.Application.Mapping
         {
             CreateMap<CreateCustomerCommand, Customer>();
             CreateMap<UpdateCustomerCommand, Customer>();
+
             CreateMap<CreateDepotCommand, Depot>();
             CreateMap<UpdateDepotCommand, Depot>();
+
+            CreateMap<CreateProductCommand, Product>().ForMember(member => member.Type,
+                options => options.MapFrom(
+                    p => ProductTypeEnum.FromValue(p.TypeValue)));
+
         }
     }
 }
